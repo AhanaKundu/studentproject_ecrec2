4582111dbe8c28de4b419f160e34b91f26dc05df
AhanaKundu <ahana.job@gmail.com>
2024-06-22 23:43:32 -0700

    data tramsformation added

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -8,6 +8,8 @@ from logger import logging
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from components.data_transformation import DataTransformation, DataTransformationConfig
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join("artifacts","train.csv")
@@ -38,4 +40,6 @@ class DataIngestion:
         
 if __name__=="__main__":
     obj= DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data_path, test_data_path= obj.initiate_data_ingestion()
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data_path, test_data_path)
