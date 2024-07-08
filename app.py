12ae7b3bab983b2ea4c9f7caacbab177fca3645e
AhanaKundu <ahana.job@gmail.com>
2024-07-08 10:20:04 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template
 import numpy as np
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler 
 #from sklearn.preprocessing import data as StandardScaler
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
